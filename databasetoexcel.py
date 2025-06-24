685e48c13250ca84cd431f73afc37d58a167ef4f
Vaibhav Yadav <vaibhavyadav7777777@gmail.com>
2025-06-24 21:51:20 +0530

    databasetoexcel

--- databasetoexcel.py
+++ databasetoexcel.py
@@ -2,11 +2,11 @@ import pandas as pd
 import pymysql
 
 connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='root',
-                             database='employees')
+                             user='root',       #your database name
+                             password='root',      #your database password
+                             database='employees')  #your data base name
 
-query = "SELECT * FROM emp"
+query = "SELECT * FROM emp" #your table name
 
 data = pd.read_sql(query, connection)
 
